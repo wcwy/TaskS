3d566f8d5de951f0f665c725fbaa3f5129945b96
wcwy <thinhong99@gmail.com>
2022-09-16 17:48:24 +0800

    Use of lambdas

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -5,7 +5,10 @@ import se.edu.inclass.task.Deadline;
 import se.edu.inclass.task.Task;
 import se.edu.inclass.task.TaskNameComparator;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
@@ -18,9 +21,13 @@ public class Main {
 //        System.out.println("All data:");
 //        printData(tasksData);
 
-//        System.out.println("Printing deadlines");
-//        printDeadlines(tasksData);
+        //System.out.println("Printing deadlines using loop");
+        //printDeadlines(tasksData);
 
+        printDeadlinesUsingStream(tasksData);
+        ArrayList<Task> filteredList = filterTaskByString(tasksData, "11");
+        System.out.println("Data with Stirng 11:");
+        printData(filteredList);
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
     }
@@ -47,5 +54,24 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks){
+        System.out.println("Printing sorted deadlines");
+        tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .sorted((a,b) -> {
+                    //System.out.println("Lemme check");
+                    return a.getDescription().compareToIgnoreCase(b.getDescription());
+                })
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterTaskByString(ArrayList<Task> tasks, String filterString){
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream()
+                .filter(t -> t.getDescription().contains(filterString))
+                .collect(toList());
+
+        return  filteredList;
     }
 }
